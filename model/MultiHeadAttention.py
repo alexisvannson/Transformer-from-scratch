892ad26fad35b4a9345a08b50ae727b478d05b77
alexisvannson <vannson.alexis@gmail.com>
2026-09-10 23:17:24 +0200

    added positional encoding, refactored code

--- model/MultiHeadAttention.py
+++ model/MultiHeadAttention.py
@@ -1,96 +1,134 @@
 import torch
 import torch.nn as nn
-import multiprocessing as mp
-    
+from math import sqrt, sin, cos
+import torch.nn.functional as F
 
-X = torch.randn(10, 512)
-
-W = torch.randn(512, 1536)
-Wq = torch.randn(512, 64)
-Wk = torch.randn(512, 64)
-Wv = torch.randn(512, 64)
-
-
-
-# shape 10 x 64
-Q = torch.matmul(X, Wq)
-K = torch.matmul(X, Wk)
-V = torch.matmul(X, Wv)
-
-FusedQKVTensor = torch.matmul(X, W) # shape 10 x 1536
-FusedQKVTensor = FusedQKVTensor.chunk(3, dim=-1) # shape 10 x  3 x 512
-FusedQKVTensor = FusedQKVTensor.reshape(10, 3, 8, 64)
-#[QKV=3, Heads=8, SeqLen=100, HeadDim=64]
-
-#linear projection 8 times (done once with matrix multiplication)
-
-#concatenate all => shape 10 x 512 then multiply by Wo to get new X
-
-
+#requires_grad_=True
 class FFN(nn.Module):
     def __init__(self, input_dim=512, hidden_dim=512, out_dim=512):
-        super().__init__
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.out_dim = out_dim
+        super().__init__()
         self.relu = nn.ReLU()
-        self.linear1 = nn.Linear(in_features=self.input_dim, out_features=self.hidden_dim)
-        self.linear2 = nn.Linear(in_features=self.hidden_dim, out_features=out_dim)
-    
+        self.linear1 = nn.Linear(in_features=input_dim, out_features=hidden_dim)
+        self.linear2 = nn.Linear(in_features=hidden_dim, out_features=out_dim)
+
     def forward(self, x):
         x = self.relu(self.linear1(x))
         x =  self.linear2(x)
         return x
+
+class DotProductAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.dk = 64
         
-        
-class Transformer(nn.Module):
-    def __init__(self, X, matrixQ, matrixK, matrixV, h=8):
-        super().__init__
-        self.Q = torch.matmul(X, matrixQ)
-        self.K = torch.matmul(X, matrixK)
-        self.V = torch.matmul(X, matrixV)
-        self.h = h
-        self.linear_layer = nn.Linear()
-    
-    def updateX(self, X):
-        self.X = X
-    def updateQ(self, Q):
-            self.Q = Q
-    def updateK(self, K):
-            self.K = K
-    def updateV(self, V):
-            self.V = V
-            
     def DotProductAttention(self, Q, K, V):
-        coefs = nn.Softmax(torch.matmul(Q, torch.transpose(K, 0, 1)), dim=1) #lacks division by sqrt(dk)
+        coefs = F.softmax(torch.matmul(Q, torch.transpose(K, 0, 1)) * (1 / sqrt(self.dk)), dim=1) # softmax applied horizontally
         X = torch.matmul(coefs, V)
         return X
-
-    def get_projections(self):
-        torch.stack(self.linear(self.Q))
-        torch.stack(self.linear(self.K))
-        torch.stack(self.linear(self.V))
-        
-    def MultiHeadAttention(self, Q, K, V):
-        with mp.Pool() as pool:
-            pool.map(self.get_projections(), range(self.h))
-        
-        with mp.Pool() as pool:
-            pool.map(self.DotProductAttention(), range(self.h)) # does that work with dimentions?!
-            
-        torch.concat   #to be fixed
-        self.linear_layer()
-        
-    def addNorm(self):
+    
+    def forward(self, x):
         pass
-    def EncoderBlock(self,x ):
-        self.MultiHeadAttention()
-        self.addNorm()
-        self.linear_layer()
-        self.addNorm()
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(in_features=64, out_features=64)
+        self.DotProductAttention = DotProductAttention()
+        self.heads = 8
+        self.embedding_dim= 512
+    
+    def get_projections(self, Q, K, V):
+        print("linear ", self.linear(Q).shape, self.linear(K).shape, self.linear(V).shape,)
+        return self.linear(Q), self.linear(K), self.linear(V)
         
-    #captum 
-    #interpreto
+    def MultiHeadAttention(self, X, W, Wo):
+        FusedQKVTensor = torch.matmul(X, W) # shape: (tokens, 1536)
+        
+        token_number = FusedQKVTensor.shape[0]
+        # Unpack the tuple into separate Q, K, and V tensors, each shape (tokens, 512)
+        Q, K, V = FusedQKVTensor.chunk(3, dim=-1)  
+
+        # Reshape them for the 8 heads, then transpose each one so the Head dimension is first => shape: (8, tokens, 64)
+        Q = Q.reshape(token_number, self.heads, self.embedding_dim // self.heads).transpose(0, 1) 
+        K = K.reshape(token_number, self.heads, self.embedding_dim // self.heads).transpose(0, 1)
+        V = V.reshape(token_number, self.heads, self.embedding_dim // self.heads).transpose(0, 1)
+        
+        projectedq1, projectedk1, projectedv1 = self.get_projections(Q[0], K[0], V[0]) #10, 64
+        
+        #parallelize later
+        final_tensor = self.DotProductAttention(projectedq1, projectedk1, projectedv1) #[10, 64]
+        
+        for i in range(1, self.heads):
+            print(i)
+            projectedQ, projectedK, projectedV = self.get_projections(Q[i], K[i], V[i])
+            current = self.DotProductAttention(projectedQ, projectedK, projectedV)
+            final_tensor = torch.cat((final_tensor, current), dim=1)
+            print(final_tensor.shape)
+        
+        return torch.matmul(final_tensor, Wo)
+    
+    def MaskedMultiHeadAttention(self, Q, K, V):
+        # Assuming Q shape is (Batch_Size, Seq_Len, Embed_Dim)
+        seq_len = Q.size(1)
+        causal_mask = nn.Transformer.generate_square_subsequent_mask(seq_len)
+        return self.MultiHeadAttention(Q, K, V) + causal_mask
+    
+    def forward(self, x):
+            pass
+
+class Encoder(nn.Module):
+    def __init__(self, embedding_dim=512):
+        super().__init__()
+        self.FFN = FFN()
+        self.layer_norm = nn.LayerNorm(embedding_dim)
+        self.MultiHeadAttention = MultiHeadAttention()
+    
+    def AddAndNorm(self, initialX, finalX):
+        X = initialX + finalX
+        X = self.layer_norm(X)
+        return X
+        
+    def EncoderBlock(self,X, W, Wo):
+        x = self.AddAndNorm(X, self.MultiHeadAttention(X, W, Wo))
+        x = self.AddAndNorm(x, self.FFN(x))
+        return X
+    
+    def forward(self, x):
+        pass
+            
+class PositionalEncoding:
+    def __init__(self):
+        self.d_model = 64
+    
+    def getPositionalEncoding(self, pos, i): #could be replace by pytorch dict
+        if (i % 2 == 0):
+            return sin(pos/ (10000 ** (2*i / self.d_model)))
+        else:
+            return cos(pos/ (10000 ** (2*i / self.d_model)))
+        
+    def AddPositionalEncodings(self, words_embeddings, pos):
+        for word_embedding in words_embeddings:
+            for i in range(len(word_embedding)):
+                word_embedding[i] += self.getPositionalEncoding(pos, i)
+        return words_embeddings
         
     
-    
+class Transformer(nn.Module):
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
