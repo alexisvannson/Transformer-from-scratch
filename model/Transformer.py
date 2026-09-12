84d0abd3a00432a20bb1718833a6844c76da0abb
alexisvannson <vannson.alexis@gmail.com>
2026-09-12 11:10:47 +0200

    feat: completed Transformer with decoder

--- model/Transformer.py
+++ model/Transformer.py
@@ -122,17 +122,38 @@ class Encoder(nn.Module):
         X, K, V = self.final_block(X)
         return X, K, V
 
-class MaskedMultiHeadAttention(nn.Module):
-    def __init__(self, W, Wo):
-        super().__init__()
-        self.W = W #shape is wrong 
-        self.Wo = Wo    
-        self.multiheadAttention = MultiHeadAttention(self.W, self.Wo)
+
+class MaskedMultiHeadAttention(MultiHeadAttention):
+    def __init__(self, embedding_dim=512):
+        super().__init__(embedding_dim)
+        self.dk = 64
+    def getMask(self, dim):
+        mask = torch.zeros((dim, dim))
+        for i in range(dim):
+            for j in range(dim):
+                if (i < j):
+                    mask[i][j] = float('-inf')
+        return mask
     
+    def MaskedDotProductAttention(self, Mask, Q, K, V):
+            coefs = F.softmax((torch.matmul(Q, torch.transpose(K, 0, 1)) * (1 / sqrt(self.dk))) + Mask, dim=1) # softmax applied horizontally
+            X = torch.matmul(coefs, V)
+            return X
+        
     def forward(self,  X):
-        seq_len = X.shape[0]
-        causal_mask = nn.Transformer.generate_square_subsequent_mask(seq_len)
-        return self.MultiHeadAttention(X) + causal_mask
+        causal_mask = self.getMask(X.shape[0])
+        Q, K, V = self.reshapeProjectedFusedQKVTensor(X)
+            
+        #parallelize later
+        final_tensor = self.MaskedDotProductAttention(causal_mask, Q[0], K[0], V[0])
+
+        for i in range(1, self.heads):
+            current = self.MaskedDotProductAttention(causal_mask, Q[i], K[i], V[i])
+            final_tensor = torch.cat((final_tensor, current), dim=1)
+
+        X = torch.matmul(final_tensor, self.Wo) 
+        
+        return X, Q
            
 class PositionalEncoding:
     def __init__(self):
@@ -154,25 +175,8 @@ class PositionalEncoding:
 class CrossAttention(MultiHeadAttention):   
     def __init__(self):
         super().__init__()
-    def forward(x):
-        pass
     
-    def reshapeProjectedFusedQKVTensor(self, X):
-            FusedQKVTensor = torch.matmul(X, self.W) # shape: (tokens, 1536)
-                    
-            token_number = FusedQKVTensor.shape[0]
-            # Unpack the tuple into separate Q, K, and V tensors, each shape (tokens, 512)
-            Q, K, V = FusedQKVTensor.chunk(3, dim=-1)  
-
-            # Reshape them for the 8 heads, then transpose each one so the Head dimension is first => shape: (8, tokens, 64)
-            Q = Q.reshape(token_number, self.heads, self.embedding_dim // self.heads).transpose(0, 1) 
-            K = K.reshape(token_number, self.heads, self.embedding_dim // self.heads).transpose(0, 1)
-            V = V.reshape(token_number, self.heads, self.embedding_dim // self.heads).transpose(0, 1)
-            return Q, K, V
-        
-    def forward(self, X):
-        Q, K, V = self.reshapeProjectedFusedQKVTensor(X)
-        
+    def forward(self, X, Q, K, V ):
         #parallelize later
         final_tensor = self.DotProductAttention(Q[0], K[0], V[0]) #[10, 64]
         
@@ -190,6 +194,7 @@ class DecoderBlock(nn.Module):
         self.FFN = FFN()
         self.layer_norm = nn.LayerNorm(embedding_dim)
         self.MaskedMultiHeadAttention = MaskedMultiHeadAttention()
+        self.CrossAttention = CrossAttention()
             
     def AddAndNorm(self, initialX, finalX):
         X = initialX + finalX
@@ -197,23 +202,35 @@ class DecoderBlock(nn.Module):
         return X
 
     def forward(self, X):
-        X = self.layer_norm(X, self.MaskedMultiHeadAttention(X))
-        
-        
-        
+        X1, Q = self.MaskedMultiHeadAttention(X)
+        #print("Maskedattentionresult", X1)
+        X = self.AddAndNorm(X,  X1)
+        #print("AddNorm", X)
+        X = self.AddAndNorm(X, self.CrossAttention(X, Q, self.K, self.V))
+        X = self.AddAndNorm(X, self.FFN(X))
+        print("decoderblock result", X)
         return X
 
-class Decoder:
-    pass
+class Decoder(nn.Module):
+    def __init__(self, K, V, n_blocks=6):
+        super().__init__()
+        DecoderBlocks = [DecoderBlock(K, V) for _ in range(n_blocks)]
+        self.DecoderBlocks = nn.Sequential(*DecoderBlocks)
+        
+    def forward(self, X):
+        X = self.DecoderBlocks(X)
+        return X
+
 class Transformer(nn.Module):
     def __init__(self, out_dim,  emmbedding_dim=512):
+        super().__init__()
         self.encoder = Encoder()
-        self.decoder = Decoder()
         self.linear = nn.Linear(emmbedding_dim, out_dim)
-        
     
     def forward(self, X):
         X, K, V = self.encoder(X)
-        X = self.decoder(X, K, V)
+        decoder =  Decoder(K, V)
+        X = decoder(X)
+        X = self.linear(X)
         return F.softmax(X, dim=1)
         
